513b8f03b48efba381a198d410473883cc4f5aa5
bobigod6 <63418364+bobigod6@users.noreply.github.com>
2023-11-02 13:14:19 +0200

    9/9

--- lb6/pr8.c
+++ lb6/pr8.c
@@ -4,8 +4,36 @@
 ordine  crescatoare.    Programul  trebuie  scris  in  asa 
  fel  incat  numerele  se  citesc  de  la  tastatura 
 unul cate unul si dupa fiecare citire ele sunt ordonate*/
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Schimbă elementele arr[j] și arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
  int main()
- {
-    
+ {  
+     int n ,a[101];
+     int lungime = 0 ; 
+      printf("Introduceti un numar:");
+     scanf("%d",&n);
+      a[lungime++] = n ; 
+     while(n)
+     {
+        scanf("%d",&n);
+        a[lungime++] = n ; 
+        bubbleSort(a,lungime);
+     }
+
+      for(int i = 1 ; i < lungime;i++)
+      {
+         printf("%d ",a[i]);
+      }
     return 0 ;
  }
--- lb6/pr9.c
+++ lb6/pr9.c
@@ -1,1 +1,24 @@
- 
+ #include <stdio.h>
+ #include<string.h>
+ /*Un  sir  care  citit  de  la  cap  la  coada  si  de  la  coada
+   la  cap  este  acelasi  se  numeste  palindrom 
+(exemplu:  radar ,gigi).  Scrieti  un  program  care  citeste  un  sir 
+ de  caractere  de  la  tastatura  si  decide 
+daca este sau nu palindrom*/
+ int main()
+ {
+     char s[101] ; printf("Introduceti un cuvant:");
+     scanf("%s",&s);
+     int ok = 1; 
+     int len = strlen(s) - 1  ; 
+     int i =  0 ; 
+     int j  = len ; 
+     while(i<len/2 && j>len/2)
+     {
+      if (s[i++] != s[j--]){ok=0;break;}
+     }
+     if(ok){printf("e palindrom");}
+     else {printf("nu e palindrom");} 
+     
+    return 0 ;
+ }
